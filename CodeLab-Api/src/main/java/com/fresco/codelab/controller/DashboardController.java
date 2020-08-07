ec5e82228e53aec8f445292dd6b826d55151e014
shijo-jack <55213452+shijo-jack@users.noreply.github.com>
2020-08-07 18:31:52 +0530

    Update DashboardController.java

--- CodeLab-Api/src/main/java/com/fresco/codelab/controller/DashboardController.java
+++ CodeLab-Api/src/main/java/com/fresco/codelab/controller/DashboardController.java
@@ -3,8 +3,10 @@ package com.fresco.codelab.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fresco.codelab.model.CodeLabRepo;
@@ -25,10 +27,10 @@ public class DashboardController{
 	
 	@GetMapping
 	public ModelAndView viewHomePage() {
-		ModelAndView model = new ModelAndView("homepage.jsp");
-		model.addObject("myrepos", dashboardService.getCodeLabRepoWithOwnerId(userAutoGenId));
-		model.addObject("Allrepos", dashboardService.getAllCodeLabRepo());
-		return model;
+		ModelAndView modelAndView = new ModelAndView("homepage.jsp");
+		modelAndView.addObject("myrepos", dashboardService.getCodeLabRepoWithOwnerId(userAutoGenId));
+		modelAndView.addObject("allrepos", dashboardService.getAllCodeLabRepo());
+		return modelAndView;
 	}
 
 	@PostMapping("/createnewrepo")
@@ -37,6 +39,16 @@ public class DashboardController{
 			dashboardService.save(new CodeLabRepo(repo_name, userAutoGenId, null, null));
 		}
 		return new ModelAndView("redirect:/dashboard/");
+	}
+	
+	
+	@GetMapping(value = "/openrepo/{repoId}")
+	public ModelAndView openrepo(@PathVariable String repoId) {
+		ModelAndView modelAndView = new ModelAndView("repodashboardpage.jsp");
+		modelAndView.addObject("repo", dashboardService.getCodeLabUserWithUserId(userAutoGenId).get(0));
+		modelAndView.addObject("repoOwner", dashboardService.getCodeLabRepoWithRepoId(userAutoGenId).get(0));
+		return modelAndView;
+		    
 	}
 
 }
